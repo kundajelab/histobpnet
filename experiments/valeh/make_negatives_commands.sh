c4cd04b0c3f00dc79d70f0c8243310ff42978721
Valeh Valiollah Pour Amiri <4193454+watiss@users.noreply.github.com>
2025-09-17 14:09:23 -0700

    wip

---
+++ experiments/valeh/make_negatives_commands.sh
@@ -0,0 +1,8 @@
+python make_negatives.py \
+--output_dir /large_storage/goodarzilab/valehvpa/projects/scCisTrans/for_chrombpnet_tuto/negatives \
+--peaks /large_storage/goodarzilab/valehvpa/projects/scCisTrans/for_chrombpnet_tuto/peaks_no_blacklist.bed \
+--chrom_sizes /large_storage/goodarzilab/valehvpa/refs/hg38/hg38.chrom.sizes \
+--genomewide_gc /large_storage/goodarzilab/valehvpa/refs/hg38/genomewide_gc_hg38_stride_1000_inputlen_2114.bed \
+--genome /large_storage/goodarzilab/valehvpa/refs/hg38/GRCh38_no_alt_analysis_set_GCA_000001405.15.fasta \
+--blacklist_regions /large_storage/goodarzilab/valehvpa/refs/hg38/hg38.blacklist.bed.gz \
+--chr_fold_path /large_storage/goodarzilab/valehvpa/projects/scCisTrans/for_chrombpnet_tuto/splits/fold_0.json
